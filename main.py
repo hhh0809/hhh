e8250a6e34a78981a698e0909fcd0137fd4287a4
hhh0809 <142026990+hhh0809@users.noreply.github.com>
2023-08-19 16:24:28 +0800

    更新 pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-basic.show_string("Hello!")
+#-------------------------initial setup--------------------------#
+OLED12864_I2C.init(60)
+sonarbit.sonarbit_distance(Distance_Unit.DISTANCE_UNIT_MM, DigitalPin.P0)
+wuKong.set_servo_angle(wuKong.ServoTypeList._360, wuKong.ServoList.S7, 90)
+wuKong.stop_all_motor()
+
+
+def mid_detect():
+#-------------------------show distance--------------------------#
+    DIS = sonarbit.sonarbit_distance(Distance_Unit.DISTANCE_UNIT_MM, DigitalPin.P0)/10
+    OLED12864_I2C.show_string(0, 0, "DIS: "+DIS, 1)
+    basic.pause(100)
+#-----------------------hit wall and stop------------------------#
+    if DIS>=10 and DIS<15:
+        wuKong.stop_all_motor()
+        OLED12864_I2C.show_string(0, 0, "AHHH!      ", 1)
+        OLED12864_I2C.show_string(0, 1, "MID: "+DIS, 1)
+#-------------------------turn left------------------------------#
+        wuKong.set_servo_angle(wuKong.ServoTypeList._360, wuKong.ServoList.S7, 140)
+        basic.pause(3000)
+        LEFT = sonarbit.sonarbit_distance(Distance_Unit.DISTANCE_UNIT_MM, DigitalPin.P0)/10
+        OLED12864_I2C.show_string(0, 2, "LEFT: "+LEFT, 1)
+#-------------------------turn right-----------------------------#
+        wuKong.set_servo_angle(wuKong.ServoTypeList._360, wuKong.ServoList.S7, 40)
+        basic.pause(3000)
+        RIGHT = sonarbit.sonarbit_distance(Distance_Unit.DISTANCE_UNIT_MM, DigitalPin.P0)/10
+        OLED12864_I2C.show_string(0, 3, "RIGHT: "+RIGHT, 1)
+        basic.pause(3000)
+#-----------------------compare and turn-------------------------#
+        if RIGHT>=LEFT:
+            OLED12864_I2C.show_string(0, 0, "RIGHT!      ", 1)
+            wuKong.set_all_motor(20, 0)
+            basic.pause(500)
+            wuKong.stop_all_motor()
+        else:
+            OLED12864_I2C.show_string(0, 0, "LEFT!       ", 1)
+            wuKong.set_all_motor(0, -20)
+            basic.pause(500)
+            wuKong.stop_all_motor()
+
+        OLED12864_I2C.clear()
+#-------------------------no wall and go-------------------------#
+    else:
+        OLED12864_I2C.show_string(0, 1, "GOOD!", 1)
+        wuKong.set_servo_angle(wuKong.ServoTypeList._360, wuKong.ServoList.S7, 90)
+        wuKong.set_all_motor(-12,14)
+#----------------------------run main----------------------------#
+basic.forever(mid_detect)
--- main.ts
+++ main.ts
@@ -1,1 +1,51 @@
-basic.showString("Hello!")
+// -------------------------initial setup--------------------------#
+OLED12864_I2C.init(60)
+sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P0)
+wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S7, 90)
+wuKong.stopAllMotor()
+// ----------------------------run main----------------------------#
+basic.forever(function mid_detect() {
+    let LEFT: number;
+    let RIGHT: number;
+    // -------------------------show distance--------------------------#
+    let DIS = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P0) / 10
+    OLED12864_I2C.showString(0, 0, "DIS: " + DIS, 1)
+    basic.pause(100)
+    // -----------------------hit wall and stop------------------------#
+    if (DIS >= 10 && DIS < 15) {
+        wuKong.stopAllMotor()
+        OLED12864_I2C.showString(0, 0, "AHHH!      ", 1)
+        OLED12864_I2C.showString(0, 1, "MID: " + DIS, 1)
+        // -------------------------turn left------------------------------#
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S7, 140)
+        basic.pause(3000)
+        LEFT = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P0) / 10
+        OLED12864_I2C.showString(0, 2, "LEFT: " + LEFT, 1)
+        // -------------------------turn right-----------------------------#
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S7, 40)
+        basic.pause(3000)
+        RIGHT = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P0) / 10
+        OLED12864_I2C.showString(0, 3, "RIGHT: " + RIGHT, 1)
+        basic.pause(3000)
+        // -----------------------compare and turn-------------------------#
+        if (RIGHT >= LEFT) {
+            OLED12864_I2C.showString(0, 0, "RIGHT!      ", 1)
+            wuKong.setAllMotor(20, 0)
+            basic.pause(500)
+            wuKong.stopAllMotor()
+        } else {
+            OLED12864_I2C.showString(0, 0, "LEFT!       ", 1)
+            wuKong.setAllMotor(0, -20)
+            basic.pause(500)
+            wuKong.stopAllMotor()
+        }
+        
+        OLED12864_I2C.clear()
+    } else {
+        // -------------------------no wall and go-------------------------#
+        OLED12864_I2C.showString(0, 1, "GOOD!", 1)
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S7, 90)
+        wuKong.setAllMotor(-12, 14)
+    }
+    
+})
